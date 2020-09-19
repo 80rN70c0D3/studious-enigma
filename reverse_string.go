a3664afb36d2788d4db89b3c149578a23d17b80e
アレックス <56805446+d0p4m1n3@users.noreply.github.com>
2020-09-19 21:37:22 +0200

    Create reverse_string.go

---
+++ reverse_string.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+//ReverseString reverse the input string and return its length
+func ReverseString(word string) (ans string, ansLen int) {
+	for _, v := range word {
+		ans = string(v) + ans
+		println(ans)
+	}
+	ansLen = len(ans)
+	return
+}
+
+func main() {
+	var ans, ansLen = ReverseString("abcdefghijklmnopqrstuvwxyz")
+	fmt.Println(
+		ReverseString("abcdefghijklmnopqrstuvwxyz"),
+	)
+	fmt.Printf("Reverse string is: %s\n", ans)
+	fmt.Printf("String length is: %d\n", ansLen)
+}
